87474f80cc56b4a5a210c7ab828fb6b5d5293424
Brandon Miller <bmiller@pesi.com>
2023-12-05 11:02:32 -0600

    Complete Day03 Part 2

--- 2023/Day03/Program.cs
+++ 2023/Day03/Program.cs
@@ -3,7 +3,8 @@
 
     public static async Task Main()
     {
-        var input = await File.ReadAllLinesAsync("Day03Input1Test.txt");
+        Console.WriteLine("Started...");
+        var input = await File.ReadAllLinesAsync("Day03Input1.txt");
         var parts = new List<Part>();
         Part currentPart = null;
 
@@ -25,7 +26,7 @@
                         currentPart.Points.Add(new Point(x, y, int.Parse(point.ToString())));
                     }
                 }
-                else 
+                else
                 {
                     if (currentPart != null)
                     {
@@ -44,7 +45,7 @@
         var total = 0;
         foreach (var part in parts)
         {
-            part.FoundSymbol = FindPartSymbol(part, input);                
+            part.FoundSymbol = FindPartSymbol(part, input);
             if (part.FoundSymbol)
             {
                 var partNumber = "";
@@ -67,7 +68,7 @@
                 }
                 writer.Write($" ");
                 if (part.FoundSymbol)
-                    writer.Write("Found");
+                    writer.Write($"Found {part.SymbolPoint.Symbol} ");
                 writer.Write(" ");
                 foreach (var point in part.Points)
                 {
@@ -79,10 +80,31 @@
             writer.WriteLine(total);
         }
 
+        //Part 2
+        var gearRatio = 0;
+        for (int y = 0; y < input.Length; y++)
+        {
+            var line = input[y];
+            for (int x = 0; x < line.Length; x++)
+            {
+                if (line[x] == '*')
+                {
+                    var symbolParts = parts.Where(_ => _.SymbolPoint?.X == x && _.SymbolPoint?.Y == y).ToList();
+                    if (symbolParts.Count() == 2)
+                    {
+                        gearRatio += symbolParts
+                            .Select(_ => int.Parse(string.Join("", _.Points.Select(_ => _.Value))))
+                            .Aggregate((x, y) => x * y);
+                    }
+                }
+            }
+        }
 
+        Console.WriteLine(total);
+        Console.WriteLine(gearRatio);
     }
 
-    private static bool FindPartSymbol(Part part, string[] input) 
+    private static bool FindPartSymbol(Part part, string[] input)
     {
         foreach (var point in part.Points)
         {
@@ -93,7 +115,7 @@
         return false;
     }
 
-    private static Point FindPointSymbol(Point point, string[] input) 
+    private static Point FindPointSymbol(Point point, string[] input)
     {
         string symbols = @"!\""#$%&'()*+,-/:;<=>?@[\]^_`{|}~";
         if (point.Y > 0 && point.X > 0)
@@ -106,45 +128,45 @@
 
         if (point.X > 0)
             if (symbols.Contains(input[point.Y][point.X - 1]))
-                return new Point(point.X - 1, point.Y, input[point.Y][point.X - 1]);    
-        
-        if ((point.Y+1) < input.Length)
+                return new Point(point.X - 1, point.Y, input[point.Y][point.X - 1]);
+
+        if ((point.Y + 1) < input.Length)
             if (symbols.Contains(input[point.Y + 1][point.X]))
                 return new Point(point.X, point.Y + 1, input[point.Y + 1][point.X]);
 
-        if ((point.X+1) < input[0].Length)
+        if ((point.X + 1) < input[0].Length)
             if (symbols.Contains(input[point.Y][point.X + 1]))
-                return new Point(point.X + 1, point.Y, input[point.Y][point.X + 1]);    
+                return new Point(point.X + 1, point.Y, input[point.Y][point.X + 1]);
 
-        if ((point.Y+1) < input.Length && (point.X+1) < input[0].Length)
+        if ((point.Y + 1) < input.Length && (point.X + 1) < input[0].Length)
             if (symbols.Contains(input[point.Y + 1][point.X + 1]))
-                return new Point(point.X + 1, point.Y + 1, input[point.Y + 1][point.X + 1]);    
+                return new Point(point.X + 1, point.Y + 1, input[point.Y + 1][point.X + 1]);
 
-        if ((point.Y+1) < input.Length && point.X > 0)
+        if ((point.Y + 1) < input.Length && point.X > 0)
             if (symbols.Contains(input[point.Y + 1][point.X - 1]))
-                return new Point(point.X - 1, point.Y + 1, input[point.Y + 1][point.X - 1]);    
+                return new Point(point.X - 1, point.Y + 1, input[point.Y + 1][point.X - 1]);
 
-        if (point.Y > 0 && (point.X+1) < input[0].Length)
+        if (point.Y > 0 && (point.X + 1) < input[0].Length)
             if (symbols.Contains(input[point.Y - 1][point.X + 1]))
-                return new Point(point.X + 1, point.Y - 1, input[point.Y - 1][point.X + 1]);    
+                return new Point(point.X + 1, point.Y - 1, input[point.Y - 1][point.X + 1]);
 
-        return null;    
+        return null;
     }
 }
 
 public class Part
 {
-    public List<Point> Points {get; set;} = new List<Point>();
+    public List<Point> Points { get; set; } = new List<Point>();
     public bool FoundSymbol = false;
-    public Point SymbolPoint {get; set;}
+    public Point SymbolPoint { get; set; }
 }
 
 public class Point
 {
-    public int X {get; set;}
-    public int Y {get; set;}
-    public int Value {get; set;}
-    public char Symbol {get; set;}
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Value { get; set; }
+    public char Symbol { get; set; }
 
     public Point(int xInput, int yInput, int value)
     {
